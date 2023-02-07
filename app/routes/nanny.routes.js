4b5cdd87240a86ddc0b3ca550ac051d264c286fd
Godwin254 <godwinbee15@gmail.com>
2023-02-07 19:07:55 +0300

    update http header option on nanny.routes

--- app/routes/nanny.routes.js
+++ app/routes/nanny.routes.js
@@ -4,10 +4,10 @@ const { filters } = require('../middlewares');
 
 module.exports = function(app){
       app.use(function(req, res, next){
-            res.header(
-                  "Access-Contol-Allow-Headers",
-                  "x-access-token, Origin, Content-Type, Accept"
-            );
+            res.header("Access-Contol-Allow-Headers","x-access-token, Origin, Content-Type, Accept");
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+            //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
       
             next();
       });
